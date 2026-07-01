b53eb33862a3c94fd066c4d2a76c8b2d0cb8926c
Daniel Bayley <dbayley@mediaferry.com>
2026-07-01 16:51:06 +0100

    Test CI

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+  print("main")
+  return True
+
+# TODO https://docs.astral.sh/uv/guides/projects
+if __name__ == "__main__":
+  main()
---
+++ test_main.py
@@ -0,0 +1,6 @@
+#import pytest
+from main import main
+
+# TODO https://jumpingrivers.com/blog/intro-to-pytest
+def test_main():
+  assert main()
